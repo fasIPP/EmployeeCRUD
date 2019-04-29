e7ac978571267f06c0b8d74768aee1026c9a653d
Andre <f.aandree@gmail.com>
2019-04-29 15:48:57 +0100

    fix exercise employeecontroller

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -3,6 +3,32 @@ var Employee = require("../models/Employee");
 
 var employeeController = {};
 
+employeeController.list = function(req, res) {
+  Employee.find({}).exec(function (err, employees) {
+    if (err) {
+      console.log("Error:", err);
+    }
+    else {
+      res.render("../views/employees/index", {employees: employees});
+    }
+  });
+};
 
+employeeController.create = function(req, res) {
+  res.render("../views/employees/create");
+};
+
+// Delete an employee
+employeeController.delete = function(req, res) {
+  Employee.remove({_id: req.params.id}, function(err) {
+    if(err) {
+      console.log(err);
+    }
+    else {
+      console.log("Employee deleted!");
+      res.redirect("/employees");
+    }
+  });
+};
 
 module.exports = employeeController;
