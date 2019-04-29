765fb120ff857d86df0540c5e4b868c70ce6976f
Andre <f.aandree@gmail.com>
2019-04-29 15:52:22 +0100

    fix exercise employeecontroller

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -18,17 +18,6 @@ employeeController.create = function(req, res) {
   res.render("../views/employees/create");
 };
 
-// Delete an employee
-employeeController.delete = function(req, res) {
-  Employee.remove({_id: req.params.id}, function(err) {
-    if(err) {
-      console.log(err);
-    }
-    else {
-      console.log("Employee deleted!");
-      res.redirect("/employees");
-    }
-  });
-};
+
 
 module.exports = employeeController;
